38746306de9bda7489580ab9ecf9ecd54935cbdc
Barney Mannerings <b.mannerings@gmail.com>
2024-05-01 23:31:02 +0200

    Add more entities for hinting, also cache them in localstorage

--- src/lib/vegadescriptors.ts
+++ src/lib/vegadescriptors.ts
@@ -2,13 +2,34 @@
 * Return descriptive strings for various Vega IDs
 */
 
-export const entities: { [key: string]: string } = {}
+export const entities: { [key: string]: string } = JSON.parse(localStorage.entityDescriptors || '{}')
 
 setTimeout(async () =>{
+  let resp: any
 
   // Asset symbols by ID
-  const resp = await (await fetch('https://api.vega.community/api/v2/assets')).json()
+  resp = await (await fetch('https://api.vega.community/api/v2/assets')).json()
   for (let edge of resp.assets.edges) {
-    entities[edge?.node?.id.toLowerCase()] = edge?.node?.details?.symbol
+    const key: string | null = edge?.node?.id.toLowerCase() 
+    const desc: string | null = edge?.node?.details?.symbol
+    if (key !== null && desc !== null) entities[key] = desc
   }
+
+  // Markets
+  resp = await (await fetch('https://api.vega.community/api/v2/markets')).json()
+  for (let edge of resp.markets.edges) {
+    const key: string | null = edge?.node?.id.toLowerCase() 
+    const desc: string | null = edge?.node?.tradableInstrument?.instrument?.code
+    if (key !== null && desc !== null) entities[key] = desc
+  }
+  
+  // Proposals
+  resp = await (await fetch('https://api.vega.community/api/v2/governances')).json()
+  for (let edge of resp.connection.edges) {
+    const key: string | null = edge?.node?.proposal?.id.toLowerCase() 
+    const desc: string | null = edge?.node?.proposal?.rationale?.title
+    if (key !== null && desc !== null) entities[key] = desc
+  }
+  
+  localStorage.setItem('entityDescriptors', JSON.stringify(entities))
 }, 0) 
